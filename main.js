416c976c2be68c4acbe2c0d8bb80bd58cdffd582
?0 <86694445+0xbdg@users.noreply.github.com>
2023-11-18 19:33:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 selected("semua");
-let buttonFilter = document.getElementById("button-filter");
-let btnFill = document.getElementsByClassName("btn-fill");
-
-for (let len = 0; len < btnFill.length; len++){
-  btnFill[len].addEventListener("click",function(){
-    let aktif = document.getElementsByClassName("active");
-    aktif[0].className = aktif[0].className.replace("active","");
-    this.className = "active";
-  });
-}
-
 function selected(get){
   let col = document.getElementsByClassName("column");
 
-  if (get === "semua") get="";
+  if (get == "semua") get="";
   for (let i = 0; i < col.length;i++){
-    rm(col[i], "show");
+    hide(col[i], "show");
     if (col[i].className.indexOf(get) > -1) add(col[i], "show");
   }
 }
@@ -29,11 +18,25 @@ function add(items, name){
   }
 }
 
-function rm(items, name){
+function hide(items, name){
   let arr1, arr2;
   arr1 = items.className.split(" ");
   arr2 = name.split(" ");
   for (let i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {items.className += " " + arr2[i];}
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
   }
+  items.className = arr1.join(" ");
+}
+
+let buttonFilter = document.getElementById("button-filter");
+let btnFill = buttonFilter.getElementsByClassName("btn-fill");
+
+for (let len = 0; len < btnFill.length; len++){
+  btnFill[len].addEventListener("click",function(){
+    let aktif = document.getElementsByClassName("active");
+    aktif[0].className = aktif[0].className.replace(" active","");
+    this.className = " active";
+  });
 }
