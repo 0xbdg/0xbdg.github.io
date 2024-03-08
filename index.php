5c18684dfb1d93f3ef04aa3283e4ab5679fe49b0
?0 <benjamindguntara@protonmail.com>
2024-03-08 17:59:44 +0700

    perubahan

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$to = "example@mail.com";
-$subject = $_POST["subject"];
-$txt = $_POST["message"];
-$headers = "Name: ". $_POST["name"] . "\r\n". "From: ".$_POST["email"] . "\r\n";
-
-mail($to,$subject,$txt,$headers);
-?>
